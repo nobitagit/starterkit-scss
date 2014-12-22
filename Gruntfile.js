9a25787af390ffb6e480b92547e1a63b3e135700
Nobita <aurelio.ogliari@gmail.com>
2014-12-22 00:13:08 +0000

    configure copy task

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    // these are the folders of the project, configured as variables
+    source: 'source', 
+    build: 'build', // this build folder is *IGNORED* by git
+    dist: 'dist',   // this build folder is *TRACKED* by git
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+
+    copy: {
+      build: {
+        files: [
+          {cwd: '<%= source %>/', expand: true, src: ['**/*'], dest: '<%= build %>/'},
+
+          // includes files within path and its sub-directories
+          //{expand: true, src: ['path/**'], dest: 'dest/'},
+
+          // makes all src relative to cwd
+          //{expand: true, cwd: 'path/', src: ['**'], dest: 'dest/'},
+
+          // flattens results to a single level
+          //{expand: true, flatten: true, src: ['path/**'], dest: 'dest/', filter: 'isFile'},
+        ]
+      },
+      dist: {
+        files: [
+          {cwd: '<%= source %>/', expand: true, src: ['**/*'], dest: '<%= dist %>/'},        
+        ]
+      }
+    }    
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.registerTask('default', ['']);
+
+  grunt.registerTask('build', ['copy:build']);
+  grunt.registerTask('dist', ['copy:dist']);
+
+};
