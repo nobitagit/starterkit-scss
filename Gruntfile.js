f6aafee018ed46793865ed3b9750e53e1593f4c1
Nobita <aurelio.ogliari@gmail.com>
2014-12-24 21:01:39 +0000

    jhint task configured

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,12 +40,20 @@ module.exports = function(grunt) {
       }
     },
 
+    jshint: {
+      options: {
+        jshintrc: true
+      },
+      all: ['Gruntfile.js', 'source/*.js', '!source/lib/**/*', 'test/**/*.js']
+    },
+
     clean: {
       build: ['<%= build %>/'],
       dist: ['<%= dist %>/']
     }    
   });
 
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');  
